c549655c01abacd077a875f9ad8ee00f00fea72c
Rohit || LearnCode With RK <71060268+LearncodeWithRk@users.noreply.github.com>
2023-01-03 15:04:42 +0530

    Add existing file

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Homepage(){
+
+    return(
+    <div>
+    <h1 className="text-4xl">Wlecome to Blog</h1>
+    <p className="text-4xl">some contact</p>
+    </div>
+
+    );
+
+}
---
+++ sanity.config.ts
@@ -0,0 +1,34 @@
+import {defineConfig,} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+import { myTheme } from './theme';
+import StudioNavbar from './components/StudioNavbar';
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  basePath:"/studio",
+  name: 'Rohit_content_studio',
+  title: 'Rohit content Studio',
+
+  projectId,
+  dataset,
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+
+  studio:{
+    components:{
+      //logo: Logo,
+      navbar : StudioNavbar,
+    }
+
+  },
+
+  theme: myTheme,
+});
